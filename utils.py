f432491688004baee6aa30c05039a4949710e2d1
Polyakov Alexey <alexey.polyakov@iticapital.ru>
2022-06-14 12:14:43 +0300

    First step

---
+++ utils.py
@@ -0,0 +1,44 @@
+import json
+
+FILENAME = "candidates.json"
+
+
+def load_candidates(filename: str) -> list[dict]:
+    """Загружаем JSON из файла"""
+    with open(filename, encoding='UTF-8') as file:
+        data = json.load(file)
+    return data
+
+
+def get_candidates() -> list[dict]:
+    """Получить кандидатов"""
+    return load_candidates(FILENAME)
+
+
+def get_candidates_by_uid(uid: int) -> dict | None:
+    """Получить кандидата по UID"""
+    candidates = get_candidates()
+    for i in candidates:
+        if i["id"] == uid:
+            return i
+    return None
+
+
+def get_candidates_by_skill(skill: str) -> list[dict]:
+    """Получить кандидатов по скилу"""
+    candidates = get_candidates()
+    result = []
+    for i in candidates:
+        if skill.lower() in i["skills"].lower().split(", "):
+            result.append(i)
+    return result
+
+
+def get_candidates_by_name(name: str) -> list[dict]:
+    """Получить кандидатов по имени"""
+    candidates = get_candidates()
+    result = []
+    for i in candidates:
+        if name.lower() in i["name"].lower().split(", "):
+            result.append(i)
+    return result
