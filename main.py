327e0a20a4e8c6f47bc35cd2d07386f4cccf8a50
Polyakov Alexey <alexey.polyakov@iticapital.ru>
2022-06-14 13:18:22 +0300

    Final

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from utils import get_candidates, get_candidates_by_uid
+from utils import get_candidates, get_candidates_by_uid, get_candidates_by_name, get_candidates_by_skill
 
 app = Flask(__name__)
 
@@ -14,6 +14,18 @@ def page_index():
 def page_candidates(uid):
     candidate = get_candidates_by_uid(uid)
     return render_template("card.html", candidate=candidate)
+
+
+@app.route("/candidate_name/<candidate_name>")
+def page_candidate_name(candidate_name):
+    candidates = get_candidates_by_name(candidate_name)
+    return render_template("search.html", candidates=candidates)
+
+
+@app.route("/skill/<skill_name>")
+def page_skills(skill_name):
+    candidates = get_candidates_by_skill(skill_name)
+    return render_template("skill.html", skill=skill_name, candidates=candidates)
 
 
 app.run()
